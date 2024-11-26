a375624ea6109abb0a71b43e8fd6f726834ff35b
Eshel Tavor <eshel.tavor@axonius.com>
2024-11-26 20:32:42 +0200

    Q2

--- Primes.java
+++ Primes.java
@@ -1,5 +1,30 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int untilNum = Integer.parseInt(args[0]);
+        boolean[] primes = new boolean[untilNum + 1];
+        for (int i = 2; i <= untilNum; i++) {
+            primes[i] = true;
+        }
+        int primeNum = 2;
+        while (primeNum <= untilNum) {
+            if (primes[primeNum]) {
+                for (int i = primeNum + 1; i <= untilNum; i++) {
+                    if (i % primeNum == 0) {
+                        primes[i] = false;
+                    }
+                }
+            }
+            primeNum++;
+        }
+        int counter = 0;
+        System.out.println("Prime numbers up to " + untilNum + ":");
+        for (int i = 2; i <= untilNum; i++) {
+            if (primes[i]) {
+                System.out.println("" + i);
+                counter++;
+            }
+        }
+        double percentage = ((double) counter / (double) untilNum) * 100;
+        System.out.println("There are " + counter + " primes between 2 and " + untilNum + " (" + (int) percentage + "% are primes)");
     }
 }
